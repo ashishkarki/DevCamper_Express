c18a80956c4ee04f8dc525fd2225643ba3ddfcd8
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-07-03 15:25:01 +0545

    Added mongoose package and a model file for it to work with the mongodb atlas

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
-const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
+
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+// load env vars 
+dotenv.config({ path: './config/config.env' })
+
+// connect to mongo db
+connectDB()
 
 // bring in route files
 const bootcamps = require('./routes/bootcamps-routes')
 
-// load env vars 
-dotenv.config({ path: './config/config.env' })
+
 
 const app = express()
 
@@ -20,6 +28,14 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${ process.env.NODE_ENV } on port ${ PORT }`))
+    console.log(`Server running in ${ process.env.NODE_ENV } on port ${ PORT }`.blue.bold))
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${ err.message }.red`)
+
+    // close server and exit process
+    server.close(() => process.exit(1))
+})
